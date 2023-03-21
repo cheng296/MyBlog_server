12292a0e8f538d525c356f181ff52395703dbf6e
zhaozhi <10664467+zhaozhi123@user.noreply.gitee.com>
2023-03-21 16:15:17 +0800

    feat: 增加创建电影接口

--- router/movie.js
+++ router/movie.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const mongoose = require('mongoose')
 
 const MovieType = {
+    movieReferrer: String,
     title: String,
     coverPicture: String,
-    movieType:String,
+    movieType: String,
 }
 const MovieModel = mongoose.model("movies", new mongoose.Schema(MovieType))
 
@@ -13,6 +14,21 @@ router.get('/get', (req, res) => {
     MovieModel.find().then(data => {
         res.send(data)
     })
+})
+
+router.post('/create', (req, res) => {
+    const { movieReferrer, title, coverPicture, movieType } = req.body;
+    MovieModel.find({ title }).then(data => {
+        if (data.length !== 0) {
+            res.send({ ok: 0, message: "该电影已存在！" })
+            return new Promise((res, rej) => { })
+        } else {
+            return MovieModel.create({ movieReferrer, title, coverPicture, movieType })
+        }
+    }).then(data => {
+        res.send({ ok: 1 })
+    }
+    ).catch(err => res.send({ ok: 0 }))
 })
 
 module.exports = router;
